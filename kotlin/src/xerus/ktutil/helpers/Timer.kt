5963a5883f4b2ef8ec87545454bfc455cd4c808c
Xerus <27jf@web.de>
2018-02-20 05:15:35 +0100

    Add Timer

---
+++ kotlin/src/xerus/ktutil/helpers/Timer.kt
@@ -0,0 +1,52 @@
+package xerus.ktutil.helpers
+
+/** times in milliseconds  */
+class Timer {
+
+    private var time: Long = System.currentTimeMillis()
+
+    fun restart() {
+        time = System.currentTimeMillis()
+    }
+
+    /** elapsed time in milliseconds  */
+    fun time(): Long =
+        System.currentTimeMillis() - time
+
+    fun printTime(msg: String): Long {
+        val time = time()
+        println(parseTime(msg, time))
+        return time
+    }
+
+    companion object {
+
+        private var t: Timer? = null
+
+        fun start() {
+            t = Timer()
+        }
+
+        /** elapsed time in milliseconds  */
+        fun runtime(): Long = t!!.time()
+
+        fun finish(msg: String = "Time"): Long {
+            if (t == null)
+                return 0
+            val time = runtime()
+            t = null
+            println(parseTime(msg, time))
+            return t!!.printTime(msg)
+        }
+
+        fun parseTime(msg: String, millis: Long): String {
+            val res = StringBuilder(msg).append(": ")
+            if (millis < 10000)
+                res.append(millis).append("m")
+            else
+                res.append(millis / 100 / 10.0)
+            return res.append("s").toString()
+        }
+    }
+
+}
