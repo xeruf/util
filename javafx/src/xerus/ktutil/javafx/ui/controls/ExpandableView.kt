da4d1b8599a9d82532fceeeb8fa6d3ada453bd08
Xerus <27jf@web.de>
2018-06-09 22:31:04 +0200

    Add ExpandableView

---
+++ javafx/src/xerus/ktutil/javafx/ui/controls/ExpandableView.kt
@@ -0,0 +1,48 @@
+package xerus.ktutil.javafx.ui.controls
+
+import javafx.collections.FXCollections
+import javafx.collections.ListChangeListener
+import javafx.scene.Node
+import javafx.scene.control.Button
+import javafx.scene.layout.GridPane
+import xerus.ktutil.javafx.onClick
+import xerus.ktutil.javafx.styleClass
+
+open class ExpandableView<T>(
+		val toNodes: (T) -> Array<Node> = {
+			(it as? Node)?.let { arrayOf(it) }
+					?: (it as? Array<Node>)
+					?: (it as? Collection<Node>)?.toTypedArray()
+					?: throw IllegalArgumentException("Type parameter is not automatically convertible to Node. " +
+							"Please specify the toNodes Parameter")
+		},
+		val rowCreator: () -> T) : GridPane() {
+	
+	val rows = FXCollections.observableArrayList<T>().apply {
+		addListener(ListChangeListener {
+			children.clear()
+			forEachIndexed { index, row -> addRow(index, *(toNodes(row) + rowButtons(index))) }
+		})
+	}
+	
+	init {
+		createRow(0)
+	}
+	
+	fun createRow(index: Int = rows.size, row: T = rowCreator()) = rows.add(index, row)
+	
+	fun removeRow(index: Int) {
+		rows.removeAt(index)
+	}
+	
+	fun rowButtons(index: Int): Array<Button> {
+		val plusButton = Button().styleClass("plus").onClick {
+			createRow(index + 1)
+		}
+		val removeButton = Button().styleClass("x").onClick {
+			removeRow(index)
+		}
+		return arrayOf(plusButton, removeButton)
+	}
+	
+}
