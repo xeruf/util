3ff03e4c0ff2a6c52b9238144855b9e4d4246f7d
Xerus <27jf@web.de>
2018-11-09 00:40:16 +0100

    Improve Timer documentation

--- kotlin/src/xerus/ktutil/helpers/Timer.kt
+++ kotlin/src/xerus/ktutil/helpers/Timer.kt
@@ -1,13 +1,18 @@
 package xerus.ktutil.helpers
 
-/** Times in milliseconds.
+import xerus.ktutil.formatTimeDynamic
+
+/**
+ * Records times in milliseconds.
  *
  * Can be used statically, which internally uses a static Timer instance,
- * or instantiated and used*/
+ * or instantiated and then used.
+ */
 class Timer {
 	
 	private var time: Long = System.currentTimeMillis()
 	
+	/** resets the time to [System.currentTimeMillis] */
 	fun restart() {
 		time = System.currentTimeMillis()
 	}
@@ -16,8 +21,10 @@ class Timer {
 	fun time(): Long =
 			System.currentTimeMillis() - time
 	
-	/** Prints the [time] using [parseTime]
-	 * @param msg the message to prepend the time with or null for no message */
+	/**
+	 * Pretty-prints the [time] using [parseTime] and returns it
+	 * @param msg the message to prepend the time with or null for no message
+	 * @return [time] */
 	fun printTime(msg: String?): Long {
 		val time = time()
 		println(parseTime(msg, time))
@@ -28,33 +35,37 @@ class Timer {
 		
 		private var t: Timer? = null
 		
+		/** Starts a new internal [Timer] */
 		fun start() {
 			t = Timer()
 		}
 		
-		/** elapsed time in milliseconds */
-		fun runtime(): Long = t!!.time()
+		/** Elapsed time in milliseconds */
+		fun runtime() = t!!.time()
 		
-		fun finish(msg: String = "Time"): Long {
-			if (t == null)
-				return 0
-			return t!!.printTime(msg).also { t = null }
-		}
+		/** Pretty-prints the elapsed time using [parseTime] and resets the internal timer */
+		fun finish(msg: String = "Time") =
+				t?.run { printTime(msg).also { t = null } } ?: 0
 		
-		/** Prints the given [millis] with a message.
-		 * If [millis] are 10000 or above, it will instead show seconds.
+		/**
+		 * Returns the given [millis] with a message.
+		 * If [millis] exceeds 10 seconds, it will show seconds with tens of seconds.
+		 * If [millis] exceeds 300 seconds, it will show a digital clock-like representation.
 		 *
 		 * @param msg the message to prepend the time with or null for no message
 		 * @param millis the milliseconds - will be postfixed by the appropriate unit */
 		fun parseTime(msg: String?, millis: Long): String {
 			val res = if (msg != null) StringBuilder(msg).append(": ") else StringBuilder()
-			if (millis < 10000)
-				res.append(millis).append('m')
-			else
-				res.append(millis / 1000)
-			if (millis < 100000)
-				res.append('.').append((millis / 100) % 10)
-			return res.append('s').toString()
+			if (millis < 300_000) {
+				if (millis < 10_000) {
+					res.append(millis).append('m')
+				} else {
+					res.append(millis / 1000).append('.').append((millis / 100) % 10)
+				}.append('s')
+			} else {
+				res.append(formatTimeDynamic(millis / 1000))
+			}
+			return res.toString()
 		}
 		
 	}
