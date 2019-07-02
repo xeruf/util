84b9622b2b7b1457c1fe463a9c888d617b75dca2
Xerus <27jf@web.de>
2019-07-02 19:48:42 +0200

    test(helpers): add ParserTest

---
+++ kotlin/test/xerus/ktutil/helpers/ParserTest.kt
@@ -0,0 +1,33 @@
+package xerus.ktutil.helpers
+
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldThrow
+import io.kotlintest.specs.StringSpec
+import sun.plugin.dom.exception.InvalidStateException
+
+class ParserTest: StringSpec({
+	val parseString = "test%ab% test %cd%"
+	
+	"split by Delimiters" {
+		Parser.Delimiters('%').apply(parseString) shouldBe listOf("test", "ab", " test ", "cd", "")
+	}
+	
+	val resultString = "testrun test go"
+	val parser = Parser('%')
+	"Parser parse" {
+		parser.parse(parseString) {
+			return@parse when(it) {
+				"ab" -> "run"
+				"cd" -> "go"
+				else -> throw InvalidStateException("")
+			}
+		} shouldBe resultString
+	}
+	
+	"Matcher" {
+		shouldThrow<ParserException> { parser.createMatcher(parseString) }
+		shouldThrow<ArrayIndexOutOfBoundsException> { parser.createMatcher(parseString, "ab", "cd").apply("run") }
+		parser.createMatcher(parseString, "ab", "cd").apply("run", "go") shouldBe resultString
+		parser.createMatcher(parseString, "cd", "ab").apply("go", "run") shouldBe resultString
+	}
+})
