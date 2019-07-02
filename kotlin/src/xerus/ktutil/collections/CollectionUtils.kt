0f8cc6759f34fe1df70ca66ebae2c91c0269a12e
Xerus <27jf@web.de>
2019-07-02 17:55:13 +0200

    docs: improve documentation for CollectionUtils

--- kotlin/src/xerus/ktutil/collections/CollectionUtils.kt
+++ kotlin/src/xerus/ktutil/collections/CollectionUtils.kt
@@ -2,12 +2,16 @@
 
 package xerus.ktutil.collections
 
+/** @return this collection or null if it is empty */
 inline fun <C : Collection<T>, T> C?.nullIfEmpty() =
 	this?.takeUnless { it.isEmpty() }
 
+/** Gets the element at [index] counted from the end.
+ * @return element at [lastIndex] - [index] */
 inline fun <T> List<T>.getReverse(index: Int) =
-	this[size - 1 - index]
+	this[lastIndex - index]
 
+/** Adds the given [elements] to this [MutableCollection]. */
 inline fun <E> MutableCollection<E>.addAll(vararg elements: E) =
 	this.addAll(elements)
 
@@ -15,13 +19,16 @@ inline fun <E> MutableCollection<E>.addAll(vararg elements: E) =
 
 /** Joins this list in a way that is natural to read for humans, using commas and `&` instead of the last comma.
  * @return - an empty string if the list is empty
- * - the first element [toString] if there is only one
- * - otherwise a string following the pattern "str(0), str(1), ... str(n-1) & str(n)" where n is the length of this List. */
+ *   - the first element [toString] if there is only one
+ *   - otherwise a string following the pattern "str(0), str(1), ... str(n-1) & str(n)" where n is the length of this List. */
 fun List<Any>.joinEnumeration(): String = when(size) {
 	0 -> ""
 	1 -> get(0).toString()
 	else -> "%s & %s".format(slice(0..size - 2).joinToString(", "), last())
 }
 
-/** Joins the given [sequences] in a human way, using commas and `&` instead of the last comma. */
+/** Joins the given [sequences] in a way that is natural to read for humans, using commas and `&` instead of the last comma.
+ * @return - an empty string if the list is empty
+ *   - the first element [toString] if there is only one
+ *   - otherwise a string following the pattern "str(0), str(1), ... str(n-1) & str(n)" where n is the length of this List.  */
 inline fun joinEnumeration(vararg sequences: Any): String = sequences.asList().joinEnumeration()
