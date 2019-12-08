e856afc832af3633a9f4eddb1b2618664b2443fa
Xerus <27jf@web.de>
2019-12-09 00:18:08 +0100

    feat(javafx): add Alert functions & improve documentation

--- javafx/src/xerus/ktutil/javafx/ui/Alerts.kt
+++ javafx/src/xerus/ktutil/javafx/ui/Alerts.kt
@@ -3,10 +3,12 @@ package xerus.ktutil.javafx.ui
 import javafx.scene.control.Alert
 import javafx.scene.control.Alert.AlertType
 import javafx.scene.control.ButtonType
+import javafx.scene.layout.Region
 import javafx.stage.Stage
 import javafx.stage.Window
 import xerus.ktutil.javafx.bindStylesheets
 import xerus.ktutil.javafx.checkFx
+import xerus.ktutil.javafx.properties.listen
 import xerus.ktutil.javafx.setPositionRelativeTo
 
 interface MessageDisplay {
@@ -20,7 +22,7 @@ interface MessageDisplay {
 	
 }
 
-interface JFXMessageDisplay : MessageDisplay {
+interface JFXMessageDisplay: MessageDisplay {
 	
 	val window: Window
 	
@@ -30,12 +32,12 @@ interface JFXMessageDisplay : MessageDisplay {
 		}
 	}
 	
-	fun showAlert(type: Alert.AlertType, title: String? = null, header: String? = null, content: String, vararg buttons: ButtonType) =
+	fun showAlert(type: AlertType, title: String? = null, header: String? = null, content: String, vararg buttons: ButtonType) =
 		window.createAlert(type, title, header, content, *buttons).apply { show() }
 	
 }
 
-fun Window?.createAlert(type: Alert.AlertType, title: String? = null, header: String? = null, content: String, vararg buttons: ButtonType) =
+fun Window?.createAlert(type: AlertType, title: String? = null, header: String? = null, content: String, vararg buttons: ButtonType) =
 	Alert(type, content, *buttons).also {
 		it.title = title
 		it.headerText = header
@@ -46,6 +48,7 @@ fun Window?.createAlert(type: Alert.AlertType, title: String? = null, header: St
 val Alert.stage
 	get() = dialogPane.scene.window as Stage
 
+/** Inits the owner of this Alert to the given [window], which will additionally set up the Position and Stylesheets. */
 fun Alert.initWindowOwner(window: Window) {
 	initOwner(window)
 	stage.setPositionRelativeTo(window)
@@ -53,3 +56,16 @@ fun Alert.initWindowOwner(window: Window) {
 		stage.bindStylesheets(window)
 }
 
+/** Tells the internal DialogPane to resize, either to its preferred size or given values. */
+fun Alert.resize(width: Double? = null, height: Double? = null) {
+	dialogPane.apply {
+		minWidth = width ?: Region.USE_PREF_SIZE
+		minHeight = height ?: Region.USE_PREF_SIZE
+	}
+}
+
+/** Shows this Alert and listens for its Result, calling the given [function] once its done. */
+fun Alert.useResultAsync(function: (ButtonType) -> Unit) {
+	show()
+	resultProperty().listen(function)
+}
